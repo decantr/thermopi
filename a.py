f149706f283c9af59a75694d36acfbfceedc068d
decantr <decantr@users.noreply.github.com>
2017-08-09 14:13:51 +0100

    first draft

---
+++ a.py
@@ -0,0 +1,22 @@
+import time
+import sys
+import datetime
+from influxdb import InfluxDBClient
+import envirohat
+
+# create the db client
+client = InfluxDBClient("localhost", "8086", "root", "root", "templog")
+
+try:
+    while True:
+        # build the json to send to db
+        json_body = [{
+            "fields": {
+                "temperature" : envirohat.weather.temperature()
+        }}]
+        # write to db
+        client.write_points(json_body)
+        # wait before next data point
+        time.sleep(1)
+except KeyboardInterrupt:
+    pass
