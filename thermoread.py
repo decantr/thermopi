b03e16fb772744577bc7c6d6c5e5baac49f43c62
decantr <decantr@users.noreply.github.com>
2017-08-17 13:42:24 +0100

    remove testing components

--- thermoread.py
+++ thermoread.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 from influxdb import InfluxDBClient
-from psutil import cpu_percent
-#from envirophat import weather
+from envirophat import weather
 
 # create the db client
 db = InfluxDBClient(host='localhost', database='test')
@@ -10,8 +9,7 @@ db = InfluxDBClient(host='localhost', database='test')
 msg = [{
 	'measurement': 'temperature',
     'fields': {
-		# 'value' : weather.temperature()
-		'value': cpu_percent()
+		'value' : weather.temperature()
 }}]
 # write to db
 db.write_points(msg)
